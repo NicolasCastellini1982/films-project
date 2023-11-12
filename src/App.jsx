48725de5445afdd5839965dab867597632fec6e0
Nicolas <nicolasrcastellini@gmail.com>
2023-11-12 15:12:13 -0300

    ruteo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Router, RouterProvider } from 'react-router-dom'
+import { appRouter } from './core/routes/app_router'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <RouterProvider  router = {appRouter} />    
+    </>
   )
 }
 
---
+++ src/core/features/login/views/login_views.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginView = () => {
+  return (
+    <div>login_view</div>
+  )
+}
+
+export default LoginView
