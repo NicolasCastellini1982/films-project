5f09cdce9efcee45dcc8ed0166a53b96e267c159
Nicolas <nicolasrcastellini@gmail.com>
2023-11-15 23:47:38 -0300

    Auth 2

--- src/core/routes/app_router.jsx
+++ src/core/routes/app_router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomeView from "../features/home/views/home_view";
 import LoginView from "../features/login/views/login_view";
 import PrivateRoute from "../auth/components/private.route";
+import PublicRoute from "../auth/components/public_route";
 
 
 
@@ -16,7 +17,13 @@ export const appRouter = createBrowserRouter([
  },
  {
     path: "/login",
-    element: <LoginView/>,
-
+    element: 
+    <PublicRoute>
+        <LoginView/>
+    </PublicRoute> 
+ },
+ {
+  path: "*",
+  element: <div>No se encontro la Pagina</div>,
  }
 ])
